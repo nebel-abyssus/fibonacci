2fc2196babfa036cfae9770108f09bd4c4ffd8fb
Nebel <124531053+nebel-abyssus@users.noreply.github.com>
2023-09-23 23:14:20 +0800

    Добавляем код на Scheme, для экстраполяции объёма кроликов
    Этот код использовался для подсчёта толщины слоя кроликов на поверхности планеты, если бы они имели те же характеристики что и кролики из задачи. Результат немного озадачивает...
    
    Добавляем только сейчас, поскольку ранее файл был просто забыт.

---
+++ code/src/main/scheme/fibonacci/extrapolation.scm
@@ -0,0 +1,23 @@
+(define (pow x n)
+	(define (qpow x n product)
+	        (cond ((= n 0) product)
+		      ((even? n) (qpow (* x x) (/ n 2) product))
+		      (else (qpow x (- n 1) (* product x)))))
+        (cond ((< n 0) (/ 1 (pow x (- n))))
+	      (else (qpow x n 1))))
+
+(define (layerVol r)
+        (- (sphereVol (+ earthR r)) earthVol))
+
+(define (sphereVol r)
+        (/ (* 4 pi (pow r 3)) 3))
+
+(define (root x n)
+        (expt x (/ 1 n)))
+
+(define pi 3.1415926535897932384626433832795)
+(define rabP 1500)
+(define rabM 1.5)
+(define km 1000)
+(define earthVol (* 1.08321 (pow 10 12) (pow km 3)))
+(define earthR (root (/ (* earthVol 3) 4 pi) 3))
