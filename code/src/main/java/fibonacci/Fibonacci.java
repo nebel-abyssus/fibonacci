55dd167dd9966e99204d6303faae90e5ad3c66e0
Nebel <124531053+nebel-abyssus@users.noreply.github.com>
2023-09-23 19:15:10 +0800

    Добавляем каркас класса на Java

---
+++ code/src/main/java/fibonacci/Fibonacci.java
@@ -0,0 +1,60 @@
+package fibonacci;
+
+import java.math.BigInteger;
+
+/**
+ * Нахождение чисел Фибоначчи.
+ * <p>Класс предоставляет статический метод, для нахождения чисел Фибоначчи.</p>
+ */
+public class Fibonacci {
+
+// static methods
+
+	/**
+	 * Точка входа в приложение.
+	 * <p>Метод, предполагая, что начальным аргументом запуска указан номер желаемого числа Фибоначчи, вычисляет и выводит его в стандартный вывод.</p>
+	 * @param args Набор аргументов запуска.
+	 * @throws NullPointerException Если набор аргументов запуска не существует.
+	 * @throws ArrayIndexOutOfBoundsException Если набор аргументов запуска пуст.
+	 * @throws NumberFormatException Если начальный аргумент запуска не является строкой, содержащей запись целого числа.
+	 * @throws IllegalArgumentException Если в качестве начального аргумента запуска указанно значение {@link Integer#MIN_VALUE}.
+	 */
+	public static void main (
+		final String... args
+	) { // method body
+		final int n = Integer.parseInt(args[0]);
+		System.out.println(fib(n));
+	} // main()
+
+	/**
+	 * Число Фибоначчи под указанным номером.
+	 * <p>Метод вычисляет и возвращает число Фибоначчи под указанным номером. Номер может быть как положительным, так и отрицательным.</p>
+	 * @param n Номер числа в последовательности. Не должен являться {@link Integer#MIN_VALUE}.
+	 * @return Выбранное число Фибоначчи.
+	 * @throws IllegalArgumentException Если указанный номер равен {@link Integer#MIN_VALUE}.
+	 */
+	public static BigInteger fib (
+		final int n
+	) { // method body
+		if (n == Integer.MIN_VALUE) throw new IllegalArgumentException();
+		BigInteger answer = null; // Здесь вычисляем абсолютное значение ответа, обратившись к одному из методов.
+		if ((n < 0) && (n % 2 == 0)) {
+			answer = answer.negate();
+		} // if
+		return answer;
+	} // fib()
+
+// constructors
+
+	/**
+	 * Конструктор, предотвращающий создание экземпляров класса.
+	 * <p>Конструктор объявлен с единственной целью - предотвратить создание экземпляров класса.</p>
+	 * @throws NoSuchMethodError При любом обращении к данному конструктору.
+	 */
+	private Fibonacci (
+	) { // method body
+		throw new NoSuchMethodError();
+	} // Fibonacci()
+
+	// todo
+} // Fibonacci
---
+++ code/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * Исходники для статьи о быстром нахождении чисел Фибоначчи.
+ */
+module ru.javarush.posts.fibonacci {
+
+	exports fibonacci;
+
+} // ru.javarush.posts.fibonacci
